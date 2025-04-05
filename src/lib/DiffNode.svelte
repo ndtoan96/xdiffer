3d4d4238c77e264d5094cc2dfcaa129a18eaea11
Nguyen Duc Toan <ntoan96@gmail.com>
2025-04-05 21:31:03 +0700

    better button ux

--- src/lib/DiffNode.svelte
+++ src/lib/DiffNode.svelte
@@ -15,6 +15,8 @@
 
     let { value, set_range1, set_range2 }: Props = $props();
     let applied = $derived(appliedEdits.has(deriveKey(value)));
+    let btnText = $derived.by(inferNodeBtnText);
+
     const nodeKindMap = {
         [xdiffer.DiffNodeKind.AddedNode]: "added-node",
         [xdiffer.DiffNodeKind.AddedSubNode]: "added-subnode",
@@ -29,53 +31,76 @@
         set_range1(value.range1());
         set_range2(value.range2());
     }
+
+    function inferNodeBtnText(): string {
+        if (value.kind() === xdiffer.DiffNodeKind.AddedNode) {
+            if (applied) {
+                return "XML2";
+            } else {
+                return "None";
+            }
+        } else if (value.kind() === xdiffer.DiffNodeKind.DeletedNode) {
+            if (applied) {
+                return "None";
+            } else {
+                return "XML1";
+            }
+        } else {
+            if (applied) {
+                return "XML2";
+            } else {
+                return "XML1";
+            }
+        }
+    }
 </script>
+
 <div class="node">
     <div class="hline"></div>
-{#if value.kind() === xdiffer.DiffNodeKind.UpdatedNode || value.kind() === xdiffer.DiffNodeKind.DeletedNode || value.kind() === xdiffer.DiffNodeKind.AddedNode}
-    <a
-        href="/"
-        class={{
-            [nodeKindMap[value.kind()]]: true,
-            "diff-node": true,
-            "attribute-node": value.is_attribute(),
-            "element-node": value.is_element(),
-            "text-node": value.is_text(),
-        }}
-        onclick={onChangedNodeClick}>{value.name()}</a
-    >
-    <button
-        onclick={() => {
-            const key = deriveKey(value);
-            if (applied) {
-                appliedEdits.delete(key);
-            } else {
-                appliedEdits.set(
-                    key,
-                    new xdiffer.Change(
-                        value.range1(),
-                        value.range2(),
-                        value.insert_pos(),
-                        value.is_attribute(),
-                    ),
-                );
-            }
-        }}
-        >{#if applied}Revert{:else}Apply{/if}</button
-    >
-{:else}
-    <span
-        class={{
-            [nodeKindMap[value.kind()]]: true,
-            "attribute-node": value.is_attribute(),
-            "element-node": value.is_element(),
-            "text-node": value.is_text(),
-        }}>{value.name()}</span
-    >
-{/if}
+    {#if value.kind() === xdiffer.DiffNodeKind.UpdatedNode || value.kind() === xdiffer.DiffNodeKind.DeletedNode || value.kind() === xdiffer.DiffNodeKind.AddedNode}
+        <a
+            href="/"
+            class={{
+                [nodeKindMap[value.kind()]]: true,
+                "diff-node": true,
+                "attribute-node": value.is_attribute(),
+                "element-node": value.is_element(),
+                "text-node": value.is_text(),
+                faded: btnText == "None",
+            }}
+            onclick={onChangedNodeClick}>{value.name()}</a
+        >
+        <button
+            onclick={() => {
+                const key = deriveKey(value);
+                if (applied) {
+                    appliedEdits.delete(key);
+                } else {
+                    appliedEdits.set(
+                        key,
+                        new xdiffer.Change(
+                            value.range1(),
+                            value.range2(),
+                            value.insert_pos(),
+                            value.is_attribute(),
+                        ),
+                    );
+                }
+            }}>{btnText}</button
+        >
+    {:else}
+        <span
+            class={{
+                [nodeKindMap[value.kind()]]: true,
+                "attribute-node": value.is_attribute(),
+                "element-node": value.is_element(),
+                "text-node": value.is_text(),
+            }}>{value.name()}</span
+        >
+    {/if}
 </div>
 <!-- <button>Apply</button></span> -->
-<ul>
+<ul class={{ faded: btnText == "None" }}>
     {#each value.children() as child (child)}
         <li>
             <Self value={child} {set_range1} {set_range2} />
@@ -102,6 +127,11 @@
 
     .node {
         display: flex;
+    }
+
+    .node button {
+        min-width: 3rem;
+        margin-left: 0.5rem;
     }
 
     .hline {
@@ -134,10 +164,13 @@
     .deleted-node,
     .deleted-subnode {
         background-color: yellow;
-        opacity: 0.3;
     }
 
     .updated-node {
         background-color: yellow;
+    }
+
+    .faded {
+        opacity: 0.2;
     }
 </style>
